fefb9db235b844633e0c19fa708c3812bae12b27
USER <claudia_h@naver.com>
2021-11-29 23:54:26 +0900

    SELECT, Paging 기능

--- src/main/java/com/cos/myblog/test/DummyControllerTest.java
+++ src/main/java/com/cos/myblog/test/DummyControllerTest.java
@@ -1,10 +1,15 @@
 package com.cos.myblog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import javax.persistence.GeneratedValue;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +25,32 @@ public class DummyControllerTest {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	//한 페이지 당 2건의 데이터를 리턴받기 (페이징)
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id",direction = Direction.DESC) Pageable pageable ){
+		
+		List<User> users = userRepository.findAll(pageable).getContent();
+		
+		return users;
+
+	}
+	
 	@GetMapping("/dummy/user/{id}")
 	public User detail(@PathVariable int id) {
 		
-		User user = userRepository.findById(id).orElseThrow();
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id:"+id);
+			}
+		});
+		
 		return user;
 	}
 	
